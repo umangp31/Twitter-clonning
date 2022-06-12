2d35ed3e430781e26970133a45c55ef027931e74
umangp31 <58567586+umangp31@users.noreply.github.com>
2022-06-12 11:57:04 +0530

    profile bg modified

--- src/components/Profile_page.js
+++ src/components/Profile_page.js
@@ -17,13 +17,15 @@ const Profile_page = () => {
           {/* <svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03" style="color: rgb(239, 243, 244);"><g><path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path></g></svg> */}
           {/* <svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03" style="color: rgb(239, 243, 244);"><g><path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path></g></svg> */}
           <div className="heder_user_info">
-            <h3 className="title_user_name">Umang Patel</h3>
-            <h6 className="title_no_of_tweets">7 Tweets</h6>
+            <h3 className="title_user_name">Elon Musk</h3>
+            <h6 className="title_no_of_tweets">18.3K Tweets</h6>
           </div>
         </div>
 
         <div className="profile_page_profile">
-          <div className="profile_bg_image"></div>
+          <div className="profile_bg_image">
+            <img src="https://pbs.twimg.com/profile_banners/44196397/1576183471/1080x360" alt="" />
+          </div>
           <div className="profile_page_profile_body">
             <div className="profile_user_info">
               <div className="profile_user_img">
@@ -38,17 +40,15 @@ const Profile_page = () => {
             </div>
 
             <div className="profile_bio">
-              <h3 className="profile_bio_name">Laxhmi Cheat Fund</h3>
-              <h6 className="profile_user_name">@Anuradha</h6>
-              <h6 className="profile_joining">Joined On 1 April 2020</h6>
+              <h3 className="profile_bio_name">Elon Musk</h3>
+              <h6 className="profile_user_name">@elonmusk</h6>
+              <h6 className="profile_joining">Joined On June 2009</h6>
               <div className="profile_bio_text">
-                Co-Founder of 25-Din me Paise Double ||
-                Client Baburav
               </div>
             </div>
             <div className="profile_follow">
-              <h6 className="profile_following"> <span className="profile_following_count">5M</span>Following</h6>
-              <h6 className="profile_followers"><span className="profile_followers_count">15</span>Followers</h6>
+              <h6 className="profile_following"> <span className="profile_following_count">112</span>Following</h6>
+              <h6 className="profile_followers"><span className="profile_followers_count">97.6M</span>Followers</h6>
             </div>
             <div className="profile_page_contents">
               <h6 className="profile_page_contents_title">Tweets</h6>
