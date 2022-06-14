b83b28268925f7df9f1b6ca7d0e2b38f8b7bdd91
sahilkakwani9 <sahkakwani@gmail.com>
2022-06-14 13:57:36 +0530

    Touch detection outside popup

--- src/components/Profile_page.js
+++ src/components/Profile_page.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Profile_page.css";
 import Tweets from "./Tweets";
+import Popup from "./EditProfilePopup";
 
 const Profile_page = () => {
   useEffect(() => {
     document.title = "Bill Gates (@thebillbhai)"
 
   }, [])
+  
   const [editclicked, setEditClicked] = useState(false);
   const handleBack = () => {
     window.history.back()
@@ -69,47 +72,9 @@ const Profile_page = () => {
           </div>
         </div>
       </div>
-      {editclicked ? (<div className='edit-profile-modal'>
-        <div className="edit-profile-container">
-          <div className="edit-profile-header">
-            <svg onClick={() => setEditClicked(false)} viewBox="0 0 24 24" aria-hidden="true" class="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03"><g><path d="M13.414 12l5.793-5.793c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0L12 10.586 6.207 4.793c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 1.414L10.586 12l-5.793 5.793c-.39.39-.39 1.023 0 1.414.195.195.45.293.707.293s.512-.098.707-.293L12 13.414l5.793 5.793c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L13.414 12z"></path></g></svg>
-            <div className="side-header">
-              <h3>Edit profile</h3>
-              <button onClick={() => { setEditClicked(false) }}>Save</button>
-            </div>
-          </div>
-          <div className="profile_bg_image">
-            <img src="https://pbs.twimg.com/profile_banners/44196397/1576183471/1080x360" alt="" />
-          </div>
-          <div className="profile_user_img">
-            <img
-              src="https://cdn.lorem.space/images/face/.cache/150x150/stefan-stefancik-QXevDflbl8A-unsplash.jpg"
-              alt=""
-            />
-          </div>
-          <div className="edit-profile-body">
-            <div className="name">
-              {/* <label htmlFor="">Name</label> */}
-              <input type="text" placeholder="Name" />
-            </div>
-            <div className="bio">
-              {/* <label htmlFor="">Bio</label> */}
-              <input type="text" className="special" placeholder="Bio" />
-            </div>
-            <div className="location">
-              {/* <label htmlFor="">Location</label> */}
-              <input type="text" placeholder="Location" />
-            </div>
-            <div className="website">
-              {/* <label htmlFor="">Website</label> */}
-              <input type="" placeholder="Website" />
-            </div>
-
-
-          </div>
-        </div>
-      </div>) : (<span></span>)
-      }
+      <Popup show={editclicked} onClickOutside={() => {setEditClicked(false)}}/>
+      {/* {editclicked ? <Popup show={editclicked} onClickOutside={() => {setEditClicked(false)}}/>: (<span></span>)
+      } */}
     </div>
   );
 };
