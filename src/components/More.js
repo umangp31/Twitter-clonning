90cdc5d5e3e25182177e7b778fa92d369a274ee6
umangp31 <58567586+umangp31@users.noreply.github.com>
2022-06-17 11:53:46 +0530

    UseState to more

---
+++ src/components/More.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./More.css"
+
+const More = (props) => {
+    return (
+        <div className="MoreContainer">
+            <h1 className="Content">COntent</h1>
+            <h1 className="Content">COntent</h1>
+            <h1 className="Content">COntent</h1>
+            <h1 className="Content">COntent</h1>
+            <h1 className="Content">COntent</h1>
+            <h1 className="Content">COntent</h1>
+        </div>
+    )
+}
+
+export default More
