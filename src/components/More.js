e7aa18a3a4c8dc8e33b2203bccc2f901a5e08e76
umangp31 <58567586+umangp31@users.noreply.github.com>
2022-06-19 00:23:16 +0530

    added more section

--- src/components/More.js
+++ src/components/More.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import "./More.css"
+import { useEffect,useRef } from 'react'
 
 const More = (props) => {
+    const ref =useRef(null);
+    const { touchoutside } = props;
+  const saveClicked = () => {
+    // navigate('/');
+    touchoutside && touchoutside();
+  }
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        console.log("touchoutside");
+        touchoutside && touchoutside();
+      }
+    };
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, [touchoutside]);
+  if (!props.show)
+    return null;
+
     return (
-        <div className="MoreContainer">
-            <h1 className="Content">COntent</h1>
-            <h1 className="Content">COntent</h1>
-            <h1 className="Content">COntent</h1>
-            <h1 className="Content">COntent</h1>
-            <h1 className="Content">COntent</h1>
-            <h1 className="Content">COntent</h1>
+        <div ref={ref} className="MoreContainer">
+            <h1 className="Content"><img src="" alt="" className="contentIcons" /> Topics</h1>
+            <h1 className="Content"><img src="" alt="" className="contentIcons" /> Moments</h1>
+            <h1 className="Content"><img src="" alt="" className="contentIcons" /> Newsletters</h1>
+            <h1 className="Content"><img src="" alt="" className="contentIcons" /> Twitter for Proffessionals</h1>
+            <h1 className="Content"><img src="" alt="" className="contentIcons" /> Twitter Ads</h1>
+            <h1 className="Content"><img src="" alt="" className="contentIcons" /> Analytics</h1>
+            <h1 className="Content"><img src="" alt="" className="contentIcons" /> Settings and privacy</h1>
+            <h1 className="Content"><img src="" alt="" className="contentIcons" /> Help Center</h1>
+            <h1 className="Content"><img src="" alt="" className="contentIcons" /> Display</h1>
+            <h1 className="Content"><img src="" alt="" className="contentIcons" /> Keyboard shortcuts</h1>
         </div>
     )
 }
