aba6e1502ce5071371bbe7a93ba896cbf72f069d
umangp31 <58567586+umangp31@users.noreply.github.com>
2022-07-09 10:39:02 +0530

    M

--- src/components/MessagePanel.js
+++ src/components/MessagePanel.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './MessagePanel.css'
+import IconButton from '../Assests/IconButton'
+import { CreateIcon } from '../Assests/Icons'
 export default function MessagePanel() {
     return (
+
         <div className='MessagePannel'>
+            
+            <IconButton hoverColor="border-color"><CreateIcon></CreateIcon></IconButton>
             hello
         </div>
     )
