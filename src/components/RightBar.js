c1cee430fe073d9a407aadf2c7bba8b0cbb4af9d
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-07-26 15:47:13 +0530

    fea: message bar added

--- src/components/RightBar.js
+++ src/components/RightBar.js
@@ -1,28 +1,30 @@
-import React from 'react'
+import MessageModal from "./MessageModal"
 import "./Rightbar.css"
 import What_happening_card from './What_happening_card'
 const RightBar = () => {
     return (
-        <div className='rightbar_Container'>
-            <div className="rightbar_searchbar rightbar_box">
-                <span><input className='search_twitter' type="search" placeholder='Search Twitter' /></span>
-            </div>
-            <div className="rightbar_whats_happening rightbar_box">
-                <div className="whats_happening_content">
-                    <h3 className='whats_happening_title'>Whats happening</h3>
-                    <What_happening_card />
-                    <What_happening_card />
-                    <What_happening_card />
-                    <What_happening_card />
-                    <What_happening_card />
+        <>
+            <div className='rightbar_Container'>
+                <div className="rightbar_searchbar rightbar_box">
+                    <span><input className='search_twitter' type="search" placeholder='Search Twitter' /></span>
+                </div>
+                <div className="rightbar_whats_happening rightbar_box">
+                    <div className="whats_happening_content">
+                        <h3 className='whats_happening_title'>Whats happening</h3>
+                        <What_happening_card />
+                        <What_happening_card />
+                        <What_happening_card />
+                        <What_happening_card />
+                        <What_happening_card />
+                    </div>
+                </div>
+                <div className="rightbar_follow_whom rightbar_box">
+                    <div className="follow_whom_content">
+                        <h3 className="follow_whom_title">Who to follow</h3>
+                    </div>
                 </div>
             </div>
-            <div className="rightbar_follow_whom rightbar_box">
-                <div className="follow_whom_content">
-                    <h3 className="follow_whom_title">Who to follow</h3>
-                </div>
-            </div>
-        </div>
+        </>
     )
 }
 
