b8d09aacfa804bb36bb5d8d98cb8d66767584291
umangp31 <58567586+umangp31@users.noreply.github.com>
2022-07-29 23:55:23 +0530

    fea:modified typeMsg

--- src/components/TypeMsg.js
+++ src/components/TypeMsg.js
@@ -3,10 +3,14 @@ import { GIFUploadIcon, ImageUploadIcon } from '../Assests/Icons'
 export default function TypeMsg() {
   return (
     <div className='Write'>
+      <div className='size'>
       <ImageUploadIcon/>
       <GIFUploadIcon/>
+      </div>
+      <div>
+        <input type="text" />
+      </div>
       <input type="search" />
-      
     </div>
   )
 }
