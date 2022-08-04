0edfa1189374987cdd01627fb41e1c220aa07ee3
VIVEK SUTHAR <sutharvivek18@gmail.com>
2022-08-04 08:42:55 +0530

    fea : baseuri

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://twitter-backend-suv.herokuapp.com/api/tweet",
-  baseURL: "http://localhost:5000/api/tweet",
+  baseURL: "https://twitter-backend-suv.herokuapp.com/api/tweet",
+  // baseURL: "http://localhost:5000/api/tweet",
   headers: {
     "Content-type": "application/json",
   },
